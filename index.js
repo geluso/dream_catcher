059b755585e58b72e28807e88ab2be372469c7da
Steve Geluso <stevegeluso@gmail.com>
2021-11-19 21:34:50 -0800

    record attempt one

--- index.js
+++ index.js
@@ -5,124 +5,40 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 const app = express();
 
-let IS_CONTEST_ENABLED = false;
-let CALLER_NUMBER = 0;
-let CALLER_WIN_NUMBER = 5;
-let OPERATOR_NUMBER = '15096389326'
-let IS_WINNER_DECLARED = false;
-
 // Parse incoming POST params with Express middleware
 app.use(urlencoded({ extended: false }));
 
-app.get('/see-caller-count', (req, res) => {
-  let output = '';
-  output += '<p>contest running?: ' + (IS_CONTEST_ENABLED ? 'yes' : 'no') + '</p>'
-  output += "<div><form method='POST' action='/toggle-contest'><button>toggle contest on/off</button></form></div>"
-  output += '<p>number of calls: ' + CALLER_NUMBER + '</p>'
-  output += "<div><form method='POST' action='/reset-callers-to-zero'><button>reset callers to zero</button></form></div>"
-  output += "<div><form method='POST' action='/set-caller-winner-number'><label>winning call number: <input type='number' step='1' value='" + CALLER_WIN_NUMBER + "' name='number'/><button>set</button></label></form></div>"
-  output += "<div>operator number:" + OPERATOR_NUMBER + "</div>"
-  output += "<div><form method='POST' action='/set-operator-number'><label>set operator: <input value='" + OPERATOR_NUMBER + "' name='phonenumber'/><button>set</button></label></form></div>"
-  res.send(output);
-})
-
-app.post('/toggle-contest', (req, res) => {
-  IS_CONTEST_ENABLED = !IS_CONTEST_ENABLED
-  res.redirect('/see-caller-count')
-})
-
-app.post('/reset-callers-to-zero', (req, res) => {
-  resetCallersToZero()
-  res.redirect('/see-caller-count')
-})
-
-app.post('/set-caller-winner-number', (req, res) => {
-  const number = parseInt(req.body.number)
-  setCallerWinnerNumber(number)
-  res.redirect('/see-caller-count')
-})
-
-app.post('/set-operator-number', (req, res) => {
-  const phonenumber = parseInt(req.body.phonenumber)
-  OPERATOR_NUMBER = phonenumber  
-  res.redirect('/see-caller-count')
-})
-
-function resetCallersToZero() {
-  console.log('reset calls to zero')
-  CALLER_NUMBER = 0;
-}
-
-function setCallerWinnerNumber(number) {
-  console.log('set call win number to:', number)
-  CALLER_WIN_NUMBER = number;
-}
-
-
 app.post('/voice', (request, response) => {
-  // Use the Twilio Node.js SDK to build an XML response
   const twiml = new VoiceResponse();
 
-  // If the user entered digits, process their request
-  const digit = request.body.Digits
+  console.log("request:", req)
+  console.log("request.params:", req.params)
 
-  const isRedirected = request.url.indexOf('redirect') >= 0
-  if (!isRedirected && IS_CONTEST_ENABLED) {
-    console.log('caller number!');
-    const isWinner = incrementCallerNumber(twiml);
-    if (isWinner) {
-      // // Render the response as XML in reply to the webhook request
-      response.type('text/xml');
-      response.send(twiml.toString());
-      return
-    }
-  }
-  
-  console.log('play menu');
-  let songUrl = 'http://5tephen.com/owen/mp3real/'
-  if (digit) {
-    songUrl += digit + '.mp3'
+  // const gather = twiml.gather({
+  //   numDigits: 1,
+  // });
+
+  if (request.RecordingUrl || request.params.RecordingUrl) {
+    response.hangup();
   } else {
-    songUrl += 'menu.mp3'
+    record(response)
   }
 
-  const gather = twiml.gather({
-    numDigits: 1,
-  });
-
-  gather.play({
-    loop: 1
-  }, songUrl);
-
-  twiml.redirect('/voice?redirect');
-
-  // // Render the response as XML in reply to the webhook request
+  // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
 });
 
+function record(response) {
+  response.say({
+    voice: 'woman',
+    language: 'en-US'
+  }, 'Let me record your dream');
 
-function incrementCallerNumber(twiml) {
-  CALLER_NUMBER++;
-  console.log('caller number:', CALLER_NUMBER)
-  if (CALLER_NUMBER === CALLER_WIN_NUMBER) {
-    twiml.say('Congratulations! You are lucky caller number ' + CALLER_NUMBER + '. You won!')
-    twiml.say('Stay on the line and you\'ll be connected to an operator to collect your information');
-    twiml.dial(OPERATOR_NUMBER)
-    return true
-  } else if (CALLER_NUMBER < CALLER_WIN_NUMBER) {
-    twiml.say('You are caller number ' + CALLER_NUMBER + '. try calling again!')
-  } else {
-    twiml.say('The contest has ended! You are caller number ' + CALLER_NUMBER)
-  }
-
-  // sometimes fake another caller to psyche people up
-  // but never fake another caller on the home stretch
-  if (Math.random() < .5 && CALLER_NUMBER < (CALLER_WIN_NUMBER - 3)) {
-    CALLER_NUMBER++
-  }
-
-  return false
+  response.record({
+      timeout: 10,
+      transcribe: true
+  });
 }
 
 let port = process.env.PORT || 3000;
